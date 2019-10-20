78d0740e0c6c7dc5ba95ba9d6326105259bd2154
Antti Paloposki <antti.paloposki@eficode.com>
2019-10-20 20:48:42 +0200

    Part 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,33 @@ const Person = (props) => {
 	return <p>{props.name} {props.number}</p>
 }
 
+const Persons = (props) => {
+	return (
+		props.personsToList.map((person) => <Person key={person.name} name={person.name} number={person.number}/>)
+	)
+}
+
+const Filter = (props) => {
+	return (
+		<p>filter shown name <input value={props.search} onChange={props.handleSearchChange}/></p>
+	)
+}
+
+const PersonForm = (props) => {
+	return (
+		<>
+		<div>
+			name: <input value={props.newName} onChange={props.handleNameChange} />
+			<br/>
+			number: <input value={props.newNumber} onChange={props.handleNumberChange} />
+		</div>
+		<div>
+			<button type="submit">add</button>
+		</div>
+</>
+)
+}
+
 const App = () => {
 	const [ persons, setPersons] = useState([
 		{ name: 'Arto Hellas', number: '040-123456' },
@@ -16,8 +43,7 @@ const App = () => {
 	const [ search, setSearch ] = useState('')
 
 	const handleNameChange = (event) => {
-		setNewName(event.target.value)
-	}
+		setNewName(event.target.value) }
 
 	const handleNumberChange = (event) => {
 		setNewNumber(event.target.value)
@@ -42,25 +68,17 @@ const App = () => {
 
 	const matchingPersons = persons.filter(person => person.name.toUpperCase().includes(search.toUpperCase()))
 	const personsToList = search ? matchingPersons : persons
-	const addedPersons = personsToList.map((person) => <Person key={person.name} name={person.name} number={person.number}/>)
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			filter shown name <input value={search} onChange={handleSearchChange}/>
+			<Filter value={search} handleSearchChange={handleSearchChange}/>
 			<h2>add a new</h2>
 			<form onSubmit={addEntry}>
-				<div>
-					name: <input value={newName} onChange={handleNameChange} />
-					<br/>
-					number: <input value={newNumber} onChange={handleNumberChange} />
-				</div>
-				<div>
-					<button type="submit">add</button>
-				</div>
+				<PersonForm newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
 			</form>
 			<h2>Numbers</h2>
-			{addedPersons}
+			<Persons personsToList={personsToList}/>
 		</div>
 	)
 }
